5139d0b83f0f90cd163132b84fbb9bfeb2c0bcb5
kavalianji <147530039+kavalianji@users.noreply.github.com>
2023-11-17 13:59:17 +0530

    Delete src directory

--- src/main/java/com/bus/fare/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.bus.fare.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-
-	public ResourceNotFoundException(String error_msg) {
-		super(error_msg);
-	}
-}
--- src/main/java/com/bus/fare/repo/CustomJpaRepositroy.java
+++
@@ -1,9 +0,0 @@
-package com.bus.fare.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bus.fare.entity.CardDetails;
-
-public interface CustomJpaRepositroy extends JpaRepository<CardDetails, Integer>/* ,QuerydslPredicateExecutor<Payer> */{
-
-}
